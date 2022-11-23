0fc0c748a6497fb610824e5c1e017af775a7892c
ShaohangHung <howardhung1007@gmail.com>
2022-11-23 15:27:24 +0800

    chores

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,10 +2,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = async (app) => {
   const config = new DocumentBuilder()
-    .setTitle('TodoList')
-    .setDescription('This is a basic Swagger document.')
+    .addBearerAuth()
+    .setTitle('Todo List')
+    .setDescription('nestJS Test')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [],
+  });
+  const urlPath = `swagger`;
+  SwaggerModule.setup(urlPath, app, document);
 };
